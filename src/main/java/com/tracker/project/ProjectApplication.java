d36e5ff2b6d191204f2bdd3491c8983cb8df14c7
paulrh-dorado <paulrh@doradogames.com>
2022-07-23 18:21:29 +0200

    Added common library

--- src/main/java/com/tracker/project/ProjectApplication.java
+++ src/main/java/com/tracker/project/ProjectApplication.java
@@ -1,9 +1,12 @@
 package com.tracker.project;
 
+
+import com.tlc.tracker.TrackerCommon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@TrackerCommon
 public class ProjectApplication {
 
 	public static void main(String[] args) {
